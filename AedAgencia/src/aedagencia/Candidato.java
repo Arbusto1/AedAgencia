c90638acd20ffbde2f03ef94f0a5b7b6aa5e2a4d
Augusto Rocha Araújo <113782580+Arbusto1@users.noreply.github.com>
2023-05-24 20:51:58 -0300

    Add files via upload

--- AedAgencia/src/aedagencia/Candidato.java
+++ AedAgencia/src/aedagencia/Candidato.java
@@ -8,28 +8,33 @@ public class Candidato {
     private char sexo;
     private char sexoInteresse;
     private TadGenerica<String> areaInteresse;
-    private TadCandidato candidatosPotenciais;
+    private TadGenerica<Integer> candidatosPotenciais;
     private int qtdPotencialCandidato;
+    private int grauInteresse;
     
     //Construtor de candidato
     Candidato(long id, String nome, String estado, char sexo, 
-            char sexoInteresse, TadGenerica<String> areaInteresse) {
+            char sexoInteresse) {
         
         this.id = id;
         this.nome = nome;
         this.estado = estado;
         this.sexo = sexo;
         this.sexoInteresse = sexoInteresse;
-        this.areaInteresse = areaInteresse;
+        this.areaInteresse = new TadGenerica<String>();
+        this.candidatosPotenciais = new TadGenerica<Integer>();
     }
     
     //cria candidato com as informações pegas em outro arquivo
     public Candidato criaCand(long id, String nome, String estado, char sexo, 
             char sexoInteresse, TadGenerica<String> areaInteresse) {
         
-        Candidato cand = new Candidato(id, nome, estado, sexo, sexoInteresse, 
-                areaInteresse);
+        Candidato cand = new Candidato(id, nome, estado, sexo, sexoInteresse);
         return cand;
+    }
+    
+    public void setGrauInteresse(int grauInteresse) {
+        this.grauInteresse = grauInteresse;
     }
     
     public String getEstado() {
